b5303784bdddd9f1f5c3773b171a6414b70ddee3
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-01-24 16:37:57 +0530

    Update index.js

--- backend1/index.js
+++ backend1/index.js
@@ -25,7 +25,8 @@ if (process.env.ALLOW_LOCAL === "true") {
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://jvcs-space.netlify.app/"
     );
 }
 
